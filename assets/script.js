49301c240b95b6525a887603fb9e019ddafa1431
Justin Vititoe <jvit1985@yahoo.com>
2022-03-27 14:37:13 -0500

    added dayjs and put in variables for different times

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,52 @@
 var rightNow = moment().format('dddd');
 var currentDay = moment().format('MMMM Do YYYY');
 document.getElementById("currentDay").innerHTML = rightNow + " " + currentDay;
-var initialText = $(".form-control").val;
+//variables for different times
+var nineAm = dayjs().hour("09");
+var tenAm = dayjs().hour("10");
+var elevenAm = dayjs().hour("11");
+var twelvePm = dayjs().hour("12");
+var onePm = dayjs().hour("13");
+var twoPm = dayjs().hour("14");
+var threePm = dayjs().hour("15");
+var fourPm = dayjs().hour("16");
+var fivePm = dayjs().hour("17");
+
+//variable to hold current time
+var currentTime = dayjs().hour();
+console.log(currentTime);
+
+//variable to hold created tasks
+var tasks = {};
+
+//function to load tasks
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if(!tasks) {
+        tasks = []
+    };
+};
 
 //function to save text to local storage
 function saveTasks() {
-    localStorage.setItem("textInput", JSON.stringify(initialText));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
+//function to check current time vs work day scheduler time and add class if current, future, or past
+var checkTime = function() {
+    if (dayjs().isAfter(threePm.$H)) {
+        $(".form-control").addClass("alert alert-secondary");
+    }
+    else if (dayjs().isSame(threePm.$H)) {
+        $(".form-control").addClass("alert alert-primary");
+    }
+    else {
+        $(".form-control").addClass("alert alert-success");
+    };
+};
+
+checkTime();
 
 //event listener for save button
 $(".btn").on("click", function() {
@@ -20,14 +60,13 @@ $(".form-control").on("click", function() {
     .text()
     .trim();
 
-    var textInput = $("<textarea>")
-    .addClass("form-control")
-    .val(text);
+    var textInput = 
 
-    $(this).replaceWith(textInput);
+    tasks = $(this).replaceWith(textInput);
 
     textInput.trigger("focus");
+
 });
 
 
-
+loadTasks();
